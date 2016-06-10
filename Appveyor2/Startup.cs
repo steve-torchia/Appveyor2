8afccf482b4b0683aedd042f6f52fbf44e639cf3
Steve Torchia <storchia@ioninteractive.com>
2016-06-10 13:39:41 -0400

    Add project files.

---
+++ Appveyor2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Appveyor2.Startup))]
+namespace Appveyor2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
